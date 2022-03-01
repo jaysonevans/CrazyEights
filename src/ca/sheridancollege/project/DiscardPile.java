1f8327aebcc378b9edf2c2c836ec1981b31e8bb0
Jayson <43726909+jaysonevans@users.noreply.github.com>
2022-03-01 09:33:28 -0500

    Added the discard pile class along with test class

---
+++ src/ca/sheridancollege/project/DiscardPile.java
@@ -0,0 +1,160 @@
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * This class represents the discard pile in a game of Crazy Eights.
+ *
+ * @author Jayson Evans
+ */
+public class DiscardPile
+{
+
+    private ArrayList<CrazyEightsCard> pile = new ArrayList<>();
+    private Scanner input = new Scanner(System.in);
+    private boolean addSuccessful = false; // Used for when prompting for another card
+    
+    public DiscardPile()
+    {
+        
+    }
+    
+    public boolean isAddSuccessful()
+    {
+        return addSuccessful;
+    }
+    
+    // When adding a card to the pile
+    public void add(CrazyEightsCard card)
+    {
+        // For the first card added to the pile when starting the game
+        if (pile.isEmpty())
+        {
+            pile.add(card);
+            addSuccessful = true;
+        }
+        else 
+        {   
+            if (cardIsEight(card))
+            {
+                addEight(card);
+                addSuccessful = true;
+            }
+            else if (topCardMatches(card))
+            {
+                pile.add(card);
+                addSuccessful = true;
+            }
+            else
+            {
+                printCardDoesNotMatch(card);
+                addSuccessful = false;
+            }
+        }
+    }
+    
+    // Gets the top card in the pile
+    public CrazyEightsCard getTopCard()
+    {
+        return pile.get(pile.size() - 1);
+    }
+    
+    // Print an error for a nonmatching suit in the card
+    public void printCardDoesNotMatch(CrazyEightsCard card)
+    {
+        String output = "Error: " + card.toString() + " does not match " +
+                getTopCard().toString() + "'s suit.\nNo card was added.";
+        
+        System.out.println(output);
+    }
+    
+    // Returns true if a top card matches
+    public boolean topCardMatches(CrazyEightsCard card)
+    {
+        CrazyEightsCard topCard = getTopCard();
+        
+        return card.getSuit() == topCard.getSuit();
+    }
+    
+    // Checks if the card is an eight
+    public boolean cardIsEight(CrazyEightsCard card)
+    {
+        return card.getValue() == Value.EIGHT;
+    }
+    
+    // For adding an eight to the pile
+    public void addEight(CrazyEightsCard card)
+    {
+        boolean go = true; // sentinel value to ensure a correct suit number was entered
+        
+        // Get the suit from the user and validate it
+        while (go)
+        {
+            promptForSuit();
+            
+            int newSuit = input.nextInt(); // Get the inputted suit number
+            
+            // Find the corresponding suit
+            // If no suit matches, report an error
+            switch (newSuit)
+            {
+                case 0:
+                    card.setSuit(Suit.HEARTS);
+                    go = false;
+                    break;
+                case 1:
+                    card.setSuit(Suit.CLUBS);
+                    go = false;
+                    break;
+                case 2:
+                    card.setSuit(Suit.SPADES);
+                    go = false;
+                    break;
+                case 3:
+                    card.setSuit(Suit.DIAMONDS);
+                    go = false;
+                    break;
+                default:
+                    System.out.println("Error: must enter the correct number of the corresponding suit");
+                    break;
+            }
+        }
+        
+        pile.add(card); // Add the modified card to the pile
+    }
+    
+    // Text prompt for the user to enter a suit number
+    // Used exclusively for cards with a value of eight
+    public void promptForSuit()
+    {
+            System.out.println("What would you like the top suit to be:");
+        
+            for (Suit suit: Suit.values())
+            {
+                System.out.printf("%d:%s\n", suit.ordinal(), suit.name());
+            }
+    }
+    
+    
+    // Print the cards in the pile
+    // Mainly for testing
+    public void printPile()
+    {
+        System.out.println("The discard pile contains:");
+        for (CrazyEightsCard card: pile)
+        {
+            System.out.println(card);
+        }
+    }
+    
+    // Clear the discard pile for reuse in the stock pile up until the last card
+    public void restock()
+    {
+        CrazyEightsCard card = getTopCard();
+        
+        pile.clear();
+        
+        pile.add(card);
+    }
+}
---
+++ src/ca/sheridancollege/project/TestDiscardPile.java
@@ -0,0 +1,55 @@
+package ca.sheridancollege.project;
+
+/**
+ * This class tests the discard pile class.
+ *
+ * @author Jayson Evans
+ */
+public class TestDiscardPile
+{
+
+    public static void main(String[] args)
+    {
+        // Instantiate a discard pile
+        DiscardPile discardPile = new DiscardPile();
+        
+        // Add the first card to the pile, should not produce error
+        CrazyEightsCard card = new CrazyEightsCard(Suit.HEARTS, Value.FIVE);
+        
+        discardPile.add(card);
+        
+        discardPile.printPile();
+        
+        System.out.println();
+        
+        // Add another card with the same suit as the last card placed, should not produce an error
+        card = new CrazyEightsCard(Suit.HEARTS, Value.SEVEN);
+        
+        discardPile.add(card);
+        
+        discardPile.printPile();
+        
+        System.out.println();
+        
+        // Add another card with a different suit, should produce an error
+        card = new CrazyEightsCard(Suit.SPADES, Value.KING);
+        
+        discardPile.add(card);
+        
+        discardPile.printPile();
+        
+        System.out.println();
+        
+        // Add an eight of a random suit, should not produce an error
+        card = new CrazyEightsCard(Suit.CLUBS, Value.EIGHT);
+        
+        discardPile.add(card);
+        
+        discardPile.printPile();
+        
+        // The stock pile is empty so we'll have to restock it with cards from the discard pile
+        discardPile.restock();
+        
+        discardPile.printPile();
+    }
+}
